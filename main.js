95330cf5ae51435e570522764f48b48b7c271d27
Isabel Servin <Isabel.servin@outlook.com>
2021-09-05 21:53:54 -0700

    dispensing

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
-// YOUR CODE GOES HERE
-console.log("Hello, I'm the first and only Talking Vending Machine.");
+alert("Hello, I'm the first and only Talking Vending Machine.")
+
+let item = prompt("What would you like today?")
+let qty = prompt("How many would you like?")
+
+//generate random num for "tons"
+const genRandNum = (min, max) => {
+    return Math.floor(Math.random() * max + min)
+}
+
+//dispense item by x num of times
+const dispense = (num) => {
+    for(let counter = 1; counter <= num; counter++){
+        console.log(item)
+    }
+}
+
+let rand = genRandNum(1, 20)
+
+if(qty.includes("tons")) {
+    dispense(rand)
+} else{
+    dispense(parseInt(qty))
+}
+
+console.log("There you go! Come again!")
